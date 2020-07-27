e0fe036bdaafdd3c1075ae1f8dad183d362d3554
shuaibabubakker <shuaib7153@gmail.com>
2020-07-28 01:58:50 +0530

    section page

---
+++ lib/section.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Section extends StatelessWidget {
+  static const routeName = 'section';
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height * .965,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SizedBox(height: 130),
+                Text(
+                  "Turbo Charm",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white54,
+                  ),
+                ),
+                SizedBox(height: 90),
+                FlatButton(
+                  onPressed: () {
+                    // Navigator.pushNamed(context, );
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 60,
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    color: Theme.of(context).primaryColorLight,
+                    child: Text(
+                      "USER",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w900,
+                        color: Theme.of(context).buttonColor,
+                      ),
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    // Navigator.pushNamed(context, );
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 60,
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    color: Theme.of(context).primaryColorLight,
+                    child: Text(
+                      "SHOP",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w900,
+                        color: Theme.of(context).buttonColor,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 90),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
