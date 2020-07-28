9603b15154ea545a018cf35df11c842bbe3aa794
sooraj <sbkaiprath99@gmail.com>
2020-07-29 00:16:28 +0530

    welcome screen update #3

--- lib/section.dart
+++ lib/section.dart
@@ -106,7 +106,7 @@ class Section extends StatelessWidget {
                         TextSpan(text: 'Designed by '),
                         TextSpan(
                             text: 'SentientPi',
-                            style: new TextStyle(fontWeight: FontWeight.bold)),
+                            style: TextStyle(fontWeight: FontWeight.bold)),
                       ],
                     ),
                   ),
